d3d8fd71a9145d2f86baaf90a6c8efa3d435b8aa
Pouya Dehghani <pouyadh.dev@gmail.com>
2022-05-16 15:15:02 +0430

    add Formik and Yup to ContactForm.jsx

--- src/containers/Main/ContactForm.jsx
+++ src/containers/Main/ContactForm.jsx
@@ -1,66 +1,83 @@
-import React, { useRef, useState } from "react";
-import { FaCheckCircle, FaTimesCircle } from "react-icons/fa";
-import emailjs from "@emailjs/browser";
+import React from "react";
+import { FaTimes } from "react-icons/fa";
 import { toast } from "react-toastify";
 
-const successMessage = "Thank you, Your message sent successfully";
-const errorMessage = "Sorry, something went wrong";
+import { ErrorMessage, Field, Formik, Form } from "formik";
+import * as Yup from "yup";
+// Using formik and yup just for showcase, however it's not efficient and they are technical debts due to their huge bundle sizes for such a simple form!
+
+import emailjs from "@emailjs/browser";
+
+const contactFormSchema = Yup.object().shape({
+  name: Yup.string()
+    .min(2, "Too Short")
+    .max(50, "Too Long")
+    .required("Required"),
+  email: Yup.string().email("Invalid email").required("Required"),
+  message: Yup.string()
+    .min(5, "Too Short")
+    .max(500, "Too Long")
+    .required("Required"),
+});
+
+const contactFormInitialValues = {
+  name: "",
+  email: "",
+  message: "",
+};
+
+const CustomErrorMessage = ({ name }) => (
+  <ErrorMessage name={name}>
+    {(msg) => (
+      <span className="error">
+        <FaTimes style={{ color: "#f00" }} /> {msg}
+      </span>
+    )}
+  </ErrorMessage>
+);
+
+const handleSubmit = async (values) => {
+  const sid = process.env.REACT_APP_EMAILJS_SERVICE_ID;
+  const tid = process.env.REACT_APP_EMAILJS_TEMPLATE_ID;
+  const pk = process.env.REACT_APP_EMAILJS_PUBLIC_KEY;
+  await toast.promise(emailjs.send(sid, tid, values, pk), {
+    pending: "Sending your message",
+    success: "Thank you, Your message has been sent",
+    error: "Sorry, Something went wrong",
+  });
+};
 
 const ContactForm = () => {
-  const [isSending, setIsSending] = useState(false);
-  const [error, setError] = useState(null);
-  const formRef = useRef();
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    setIsSending(true);
-    const sid = process.env.REACT_APP_EMAILJS_SERVICE_ID;
-    const tid = process.env.REACT_APP_EMAILJS_TEMPLATE_ID;
-    const pk = process.env.REACT_APP_EMAILJS_PUBLIC_KEY;
-    toast
-      .promise(emailjs.sendForm(sid, tid, formRef.current, pk), {
-        pending: "Sending your message",
-        success: "Thank you, Your message has been sent",
-        error: "Sorry, Something went wrong",
-      })
-      .finally(() => setIsSending(false));
-
-    // emailjs
-    //   .sendForm(sid, tid, formRef.current, pk)
-    //   .then((result) => {
-    //     console.log(result);
-    //     setError(false);
-    //   })
-    //   .catch((err) => {
-    //     console.log(err);
-    //     setError(true);
-    //   })
-    //   .finally(() => setIsSending(false));
-  };
   return (
-    <form id="contact-form" ref={formRef} onSubmit={handleSubmit}>
-      <label htmlFor="name-input">Your name</label>
-      <input id="name-input" type="text" name="name" />
-      <label htmlFor="email-input">Your email</label>
-      <input id="email-input" type="email" name="email" />
-      <label htmlFor="message-input">Your message</label>
-      <textarea id="message-input" form="contact-form" name="message" />
+    <Formik
+      initialValues={contactFormInitialValues}
+      validationSchema={contactFormSchema}
+      onSubmit={handleSubmit}
+    >
+      {({ isSubmitting }) => (
+        <Form id="contact-form">
+          <label htmlFor="name-input">Your name</label>
+          <Field id="name-input" type="text" name="name" />
+          <CustomErrorMessage name="name" />
+          <label htmlFor="email-input">Your email</label>
+          <Field id="email-input" type="email" name="email" />
+          <CustomErrorMessage name="email" />
+          <label htmlFor="message-input">Your message</label>
+          <Field
+            as="textarea"
+            id="message-input"
+            form="contact-form"
+            name="message"
+          />
+          <CustomErrorMessage name="message" />
 
-      <button type="submit" name="send" disabled={isSending}>
-        {isSending && <span className="spinner" />}
-        {isSending ? "Sending..." : "Send"}
-      </button>
-      {error === false && (
-        <label>
-          <FaCheckCircle style={{ color: "#0f0" }} /> {successMessage}
-        </label>
+          <button type="submit" disabled={isSubmitting}>
+            {isSubmitting && <span className="spinner" />}
+            {isSubmitting ? "Sending..." : "Send"}
+          </button>
+        </Form>
       )}
-      {error === true && (
-        <label>
-          <FaTimesCircle style={{ color: "#f00" }} /> {errorMessage}
-        </label>
-      )}
-    </form>
+    </Formik>
   );
 };
 
