6b89a2c9019b900c9e671993c90e46a1a9e909a9
Pouya Dehghani <pouyadh.dev@gmail.com>
2022-05-23 14:54:20 +0430

    pass props to container

--- src/components/SomethingWentWrong/SomethingWentWrong.jsx
+++ src/components/SomethingWentWrong/SomethingWentWrong.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 import "./SomethingWentWrong.scss";
 
-const SomethingWentWrong = ({ h1, h2, p1, p2 }) => {
+const SomethingWentWrong = ({ h1, h2, p1, p2, ...props }) => {
   return (
-    <div className="uni-error">
+    <div className="uni-error" {...props}>
       <h1>{h1}</h1>
       <h2>{h2}</h2>
       <p>{p1}</p>
