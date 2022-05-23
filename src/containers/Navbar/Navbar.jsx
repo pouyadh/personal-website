04dc11147f21fadc05fda423021c351e8ae58fbc
Pouya Dehghani <pouyadh.dev@gmail.com>
2022-05-23 15:02:32 +0430

    add animated hamberger button for navbar, some improvements

--- src/containers/Navbar/Navbar.jsx
+++ src/containers/Navbar/Navbar.jsx
@@ -1,9 +1,89 @@
 import React, { useState } from "react";
 import "./Navbar.scss";
-import { ReactComponent as Logo } from "../../assets/logo.svg";
-import { ReactComponent as NavHambergerSVG } from "../../assets/nav-hamberger.svg";
+import { ReactComponent as LogoSVG } from "../../assets/logo.svg";
 import classNames from "classnames";
 import { FormattedMessage } from "react-intl";
+import { motion } from "framer-motion";
+
+const v = {
+  ltb: {
+    close: {
+      x1: "0%",
+      x2: "100%",
+    },
+    open: {
+      x1: "50%",
+      x2: "50%",
+    },
+  },
+  lc1: {
+    close: {
+      rotate: 0,
+    },
+    open: {
+      rotate: 45,
+    },
+  },
+  lc2: {
+    close: {
+      rotate: 0,
+    },
+    open: {
+      rotate: -45,
+    },
+  },
+};
+
+const Hamberger = ({ closed, ...props }) => (
+  <svg
+    viewBox="0 0 24 18"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+    {...props}
+  >
+    <motion.line
+      y1="1"
+      x2="24"
+      y2="1"
+      stroke="white"
+      strokeWidth="2"
+      initial="close"
+      animate={closed ? "close" : "open"}
+      variants={v.ltb}
+    />
+    <motion.line
+      y1="9"
+      x2="24"
+      y2="9"
+      stroke="white"
+      strokeWidth="2"
+      initial="close"
+      transform-origin="12px 9px"
+      animate={closed ? "close" : "open"}
+      variants={v.lc1}
+    />
+    <motion.line
+      y1="9"
+      x2="24"
+      y2="9"
+      stroke="white"
+      strokeWidth="2"
+      initial="close"
+      animate={closed ? "close" : "open"}
+      variants={v.lc2}
+    />
+    <motion.line
+      y1="17"
+      x2="24"
+      y2="17"
+      stroke="white"
+      strokeWidth="2"
+      initial="close"
+      animate={closed ? "close" : "open"}
+      variants={v.ltb}
+    />
+  </svg>
+);
 
 const Navbar = ({ locale }) => {
   const [isOpen, setIsOpen] = useState(false);
@@ -16,27 +96,35 @@ const Navbar = ({ locale }) => {
   };
   return (
     <nav className={classNames({ open: isOpen })}>
-      <Logo
-        className="logo"
-        onClick={() => window.location.replace(window.location.pathname)}
-      />
-      <ul>
-        {["projects", "skills", "about", "contact"].map((item) => (
-          <li key={`navbar-navLink-${item}`}>
-            <a href={`#${item}`} onClick={handleLinkClick}>
-              <FormattedMessage id={`app.nav.${item}`} />
-            </a>
+      <motion.div
+        className="container"
+        animate={{ height: isOpen ? "12em" : "4em" }}
+      >
+        <LogoSVG
+          width={undefined}
+          height={undefined}
+          className="logo"
+          onClick={() => window.location.replace(window.location.pathname)}
+        />
+        <Hamberger
+          className="hamberger"
+          onClick={() => setIsOpen(!isOpen)}
+          closed={!isOpen}
+        />
+        <ul>
+          {["projects", "skills", "about", "contact"].map((item) => (
+            <li key={`navbar-navLink-${item}`}>
+              <a href={`#${item}`} onClick={handleLinkClick}>
+                <FormattedMessage id={`app.nav.${item}`} />
+              </a>
+            </li>
+          ))}
+          <li>
+            {locale !== "fa" && <a href="?lang=fa">🇮🇷 فارسی</a>}
+            {locale !== "en" && <a href="?lang=en">English 🇬🇧</a>}
           </li>
-        ))}
-        <li>
-          {locale !== "fa" && <a href="?lang=fa">🇮🇷 فارسی</a>}
-          {locale !== "en" && <a href="?lang=en">English 🇬🇧</a>}
-        </li>
-      </ul>
-      <NavHambergerSVG
-        className="hamberger"
-        onClick={() => setIsOpen(!isOpen)}
-      />
+        </ul>
+      </motion.div>
     </nav>
   );
 };
